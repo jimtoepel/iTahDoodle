67263d6ad61f67c9bf5b2da7a2e0b3f5495b0a02
jimtoepel <jim.toepel@gmail.com>
2015-04-12 14:04:57 -0700

    Initial commit

---
+++ iTahDoodle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iTahDoodle
+//
+//  Created by Jim Toepel on 4/12/15.
+//  Copyright (c) 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iTahDoodle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iTahDoodle
+//
+//  Created by Jim Toepel on 4/12/15.
+//  Copyright (c) 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
